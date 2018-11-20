5b062e3292b62a10becf89cf6edb7558ec6dc706
Andrey Klimov <anklimsk@gmail.com>
2018-11-20 17:52:46 +0300

    Added plugin CakeTCPDF

---
+++ app/Plugin/CakeTCPDF/Config/routes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Routes configuration
+ *
+ * CakeTCPDF: Generate PDF files with CakePHP.
+ * @copyright Copyright 2017-2018, Andrey Klimov.
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ * @package plugin.Config
+ */
+
+	Router::parseExtensions();
+	Router::setExtensions(['pdf']);
